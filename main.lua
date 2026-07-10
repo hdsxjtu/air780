d7712a230a080876973f24bebb95f4ca66328630
DSHE <hds1988@126.com>
2026-07-10 13:36:56 +0800

    chore(4g): bump version to 1.0.76

--- main.lua
+++ main.lua
@@ -1,7 +1,7 @@
 -- Global Project Info
 -- Note: Must be defined as literals for LuatTools to recognize them
 PROJECT = "TRACKER_PRO"
-VERSION = "1.0.75"
+VERSION = "1.0.76"
 
 log.info("MAIN", "PROJECT: " .. PROJECT .. " VERSION: " .. VERSION)
 
