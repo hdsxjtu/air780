0a6a089a923eb8bf47a82f89ba6067fcc111f683
DSHE <hds1988@126.com>
2026-08-09 10:05:47 +0800

    bump: release Air780E V1.0.108

--- main.lua
+++ main.lua
@@ -1,7 +1,7 @@
 -- Global Project Info
 -- Note: Must be defined as literals for LuatTools to recognize them
 PROJECT = "TRACKER_PRO"
-VERSION = "1.0.107"
+VERSION = "1.0.108"
 
 -- Keep startup logs focused; LuatTools reads PROJECT/VERSION from the literals above.
 
